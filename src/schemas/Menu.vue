cb3b507ad05424145d286421a028697fcb7db732
LuisomDev <luisomdev@gmail.com>
2025-02-13 17:48:45 -0500

    Add text for develop

--- src/schemas/Menu.vue
+++ src/schemas/Menu.vue
@@ -11,10 +11,10 @@ const { toast } = useToast()
 const mode = useColorMode({ disableTransition: false })
 const themeText = ref(mode.value)
 
-onBeforeMount(()=> themeText.value = mode.value === 'light' ? 'dark' : 'light' )
-watch(mode, (value)=> themeText.value = value === 'light' ? 'dark' : 'light')
+onBeforeMount(() => themeText.value = mode.value === 'light' ? 'dark' : 'light')
+watch(mode, (value) => themeText.value = value === 'light' ? 'dark' : 'light')
 
-const changeTheme = ()=>{
+const changeTheme = () => {
     toast({
         title: 'Change theme to ${mode.value}',
         description: 'theme updated'
@@ -30,29 +30,39 @@ const changeTheme = ()=>{
 
         <div class="flex justify-center items-center">
             <Icon icon="mdi:bird" width="48" height="48" />
-                        <span class="text-xl font-bold px-4"> VeterinCap </span>
+            <span class="text-xl font-bold px-4"> VeterinCap </span>
 
         </div>
 
         <div class="flex">
-            <div class="px-2"> <Button variant="outline" as-child> <a href="/"> <Icon icon="mdi:home-circle-outline" />  Home </a> </Button> </div>
-            
-            <div class="px-2"> <Button variant="outline" as-child> <a href="/"> <Icon icon="mdi:account-service-outline" /> Services </a> </Button> </div>
-            <div class="px-2"> <Button variant="outline" as-child> <a href="/"> <Icon icon="mdi:notebook-edit-outline" width="24" height="24" /> Appointment </a> </Button> </div>
-            <div class="px-2"> <Button variant="outline" as-child> <a href="/"> <Icon icon="mdi:about-circle-outline" /> About us </a> </Button> </div> 
+            <div class="px-2"> <Button variant="outline" as-child> <a href="/">
+                        <Icon icon="mdi:home-circle-outline" /> Home
+                    </a> </Button> </div>
+
+            <div class="px-2"> <Button variant="outline" as-child> <a href="/">
+                        <Icon icon="mdi:account-service-outline" /> Services
+                    </a> </Button> </div>
+            <div class="px-2"> <Button variant="outline" as-child> <a href="/">
+                        <Icon icon="mdi:notebook-edit-outline" width="24" height="24" /> Appointment
+                    </a> </Button> </div>
+            <div class="px-2"> <Button variant="outline" as-child> <a href="/">
+                        <Icon icon="mdi:about-circle-outline" /> About us
+                    </a> </Button> </div>
 
             <div class="px-4">
                 <Separator orientation="vertical"></Separator>
             </div>
-            <Button @click="()=>{
+            <Button @click="() => {
                 mode = mode == 'light' ? 'dark' : 'light'
                 toast({
                     title: `Theme change to ${mode}`,
                     description: 'Updated configuration of theme'
                 })
             }">
-                
-                <Icon class="transition-all duration-500 " :class="mode == 'light' ? 'opacity-100 scale-100 rotate-0' : 'opacity-100 scale-100 rotate-180' " icon="mdi:sun-moon-stars" />
+
+                <Icon class="transition-all duration-500 "
+                    :class="mode == 'light' ? 'opacity-100 scale-100 rotate-0' : 'opacity-100 scale-100 rotate-180'"
+                    icon="mdi:sun-moon-stars" />
 
                 Theme to {{ themeText }}
             </Button>
@@ -61,6 +71,6 @@ const changeTheme = ()=>{
 
     </nav>
 
-    <Toaster/>
+    <Toaster />
 
 </template>
